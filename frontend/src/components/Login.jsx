9418604b3d1ec8183151e5a8ae1b07c51b96c459
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-02-09 14:53:36 +0530

    url update

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/api/login', formData);
+      const response = await axios.post('https://backend-login-qzxq.onrender.com/api/login', formData);
       const token = response.data.token;
   
       // Store the token in localStorage
--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -63,7 +63,7 @@ const User = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/user/`);
+        const response = await axios.get(`https://backend-login-qzxq.onrender.com/api/user/`);
         setUser(response.data);
         setFormData({ ...response.data });
         setError('');
@@ -90,12 +90,12 @@ const User = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.put(`http://localhost:3000/api/user/`, formData);
+      const response = await axios.put(`https://backend-login-qzxq.onrender.com/api/user/`, formData);
       setUser(response.data);
       setFormData({ ...response.data });
       setEditing(false);
 
-      const updatedResponse = await axios.get(`http://localhost:3000/api/user/`);
+      const updatedResponse = await axios.get(`https://backend-login-qzxq.onrender.com/api/user/`);
       setUser(updatedResponse.data);
       setFormData({ ...updatedResponse.data });
     } catch (error) {
